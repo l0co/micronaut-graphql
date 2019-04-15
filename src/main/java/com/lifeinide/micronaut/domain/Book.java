b2a3f20b7dd22efe9ed1d446c006bdd58ed5fc9e
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-04-15 09:29:40 +0200

    h2 + jpa integration

---
+++ src/main/java/com/lifeinide/micronaut/domain/Book.java
@@ -0,0 +1,84 @@
+package com.lifeinide.micronaut.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Model comes from https://guides.micronaut.io/micronaut-data-access-jpa-hibernate/guide/index.html
+ */
+@Entity
+@Table(name = "book")
+public class Book {
+
+	public Book() {
+	}
+
+	public Book(@NotNull String isbn, @NotNull String name, Genre genre) {
+		this.isbn = isbn;
+		this.name = name;
+		this.genre = genre;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@NotNull
+	@Column(name = "isbn", nullable = false)
+	private String isbn;
+
+	@ManyToOne
+	private Genre genre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Book{");
+		sb.append("id=");
+		sb.append(id);
+		sb.append(", name='");
+		sb.append(name);
+		sb.append("', isbn='");
+		sb.append(isbn);
+		sb.append("', genre='");
+		sb.append(genre);
+		sb.append("'}");
+		return sb.toString();
+	}
+}
---
+++ src/main/java/com/lifeinide/micronaut/domain/Genre.java
@@ -0,0 +1,65 @@
+package com.lifeinide.micronaut.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Model comes from https://guides.micronaut.io/micronaut-data-access-jpa-hibernate/guide/index.html
+ */
+@Entity
+@Table(name = "genre")
+public class Genre {
+
+	public Genre() {
+	}
+
+	public Genre(@NotNull String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	@Column(name = "name", nullable = false, unique = true)
+	private String name;
+
+	@OneToMany(mappedBy = "genre")
+	private Set<Book> books = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public String toString() {
+		return "Genre{" +
+			"id=" + id +
+			", name='" + name + '\'' +
+			'}';
+	}
+
+}
