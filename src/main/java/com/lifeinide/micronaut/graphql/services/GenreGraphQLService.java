97e60b3de7b7749234a7e936ff43bcc195299c7b
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-04-18 19:24:15 +0200

    custom field

--- src/main/java/com/lifeinide/micronaut/graphql/services/GenreGraphQLService.java
+++ src/main/java/com/lifeinide/micronaut/graphql/services/GenreGraphQLService.java
@@ -1,8 +1,10 @@
 package com.lifeinide.micronaut.graphql.services;
 
+import com.lifeinide.micronaut.domain.Book;
 import com.lifeinide.micronaut.domain.Genre;
 import com.lifeinide.micronaut.graphql.GraphQLService;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.micronaut.spring.tx.annotation.Transactional;
 
@@ -10,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Lukasz Frankowski
@@ -28,6 +31,11 @@ public class GenreGraphQLService {
 	@GraphQLQuery
 	public Optional<Genre> genre(@GraphQLArgument(name = "id") long id) {
 		return Optional.ofNullable(entityManager.find(Genre.class, id));
+	}
+
+	@GraphQLQuery
+	public List<Book> books(@GraphQLContext Genre genre, @GraphQLArgument(name = "limit") long limit) {
+		return genre.getBooks().stream().limit(limit).collect(Collectors.toList());
 	}
 
 }
